b9b93da7c56683dd5151b4059bb8a76955595754
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2023-10-27 15:39:16 -0400

    2A solved

---
+++ 2A.js
@@ -0,0 +1,11 @@
+// for (let i= 0; i<=10; i = i +1){
+//     console.log(i)
+// }
+
+// for (let i= 10; i<=400; i = i + 1){
+//     console.log(i)
+// }
+
+for (let i = 12; i<= 4000; i = i + 3){
+    console.log(i)
+}
